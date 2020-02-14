1949bac89d5c4e9a54ea1d42189567f39d49ab4f
rommel <rommel@rommel.local>
2020-02-14 16:42:56 -0600

    add Header and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Layout/Header';
+import Dashboard from './components/Dashboard/Dashboard/Dashboard';
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Fragment>
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+            </Switch>
+          </div>
+        </Fragment>   
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary justify-content-between d-flex mb-4">
+        <div className="container">
+            <Link to="/" className="navbar-brand text-light font-weight-bold">CRM</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navegacion" aria-controls="navegacion" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navegacion">
+                <ul className="navbar-nav ml-auto text-right">
+                <li className="nav-item dropdown mr-4">
+                        <button 
+                            className="nav-link dropdown-toggle btn btn-success"
+                            data-toggle="dropdown"
+                        >
+                            Los Productos
+                        </button>
+                        <div className="dropdown-menu" aria-labelledby="navegacion">
+                            <Link to='' className="dropdown-item">
+                                Productos
+                            </Link>
+                            <Link to='' className="dropdown-item">
+                                Nuevo Producto
+                            </Link>
+                        </div>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <button 
+                            className="nav-link dropdown-toggle btn btn-success"
+                            data-toggle="dropdown"
+                        >
+                            Los Clientes
+                        </button>
+                        <div className="dropdown-menu" aria-labelledby="navegacion">
+                            <Link to='' className="dropdown-item">
+                                Clientes
+                            </Link>
+                            <Link to='' className="dropdown-item">
+                                Nuevo Cliente
+                            </Link>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+);
+
+export default Header;
